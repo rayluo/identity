471001f9a0adc49db486d8d2a0e5ed6b4d272c0c
Ray Luo <rayluo.mba@gmail.com>
2022-09-25 14:36:37 -0700

    Incarnation
    Moving to right position

---
+++ identity/__init__.py
@@ -0,0 +1,2 @@
+from .version import __version__  # It is defined inside a module, and imported here.
+    # The other way around would introduce cyclic reference.
--- my_package/__init__.py
+++
@@ -1,2 +0,0 @@
-from .main_module import __version__  # It is defined inside a module, and imported here.
-    # The other way around would introduce cyclic reference.
