e6ec017b9b3defe032dffda2b09922cd67c2a6b9
Ray Luo <rayluo.mba@gmail.com>
2024-10-05 12:34:56 -0700

    Pallet logout shall honor APPLICATION_ROOT

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,21 +1,40 @@
+import shutil
 from unittest.mock import patch, Mock
 
+import pytest
 from flask import Flask
 
 from identity.flask import Auth
 
 
-def test_logout():
+@pytest.fixture()
+def app():  # https://flask.palletsprojects.com/en/3.0.x/testing/
     app = Flask(__name__)
-    app.config["SESSION_TYPE"] = "filesystem"  # Required for Flask-session,
-        # see also https://stackoverflow.com/questions/26080872
-    auth = Auth(app, client_id="fake", authority="https://example.com/foo")
+    app.config.update({
+        "APPLICATION_ROOT": "/app_root",  # Mimicking app with explicit root
+        "SESSION_TYPE": "filesystem",  # Required for Flask-session,
+            # see also https://stackoverflow.com/questions/26080872
+    })
+    yield app
+    shutil.rmtree("flask_session")  # clean up
+
+@pytest.fixture()
+def auth(app):
+    return Auth(
+        app,
+        client_id="fake",
+        redirect_uri="http://localhost:5000/redirect",  # To use auth code flow
+        oidc_authority="https://example.com/foo",
+    )
+
+def test_logout(app, auth):
     with patch.object(auth._auth, "_get_oidc_config", new=Mock(return_value={
         "end_session_endpoint": "https://example.com/end_session",
     })):
         with app.test_request_context("/", method="GET"):
-            assert auth._request.host_url in auth.logout().get_data(as_text=True), (
-                "The host_url should be in the logout URL. There was a bug in 0.9.0.")
+            homepage = "http://localhost/app_root"
+            assert homepage in auth.logout().get_data(as_text=True), (
+                "The homepage should be in the logout URL. There was a bug in 0.9.0.")
 
 @patch("msal.authority.tenant_discovery", new=Mock(return_value={
     "authorization_endpoint": "https://example.com/placeholder",
